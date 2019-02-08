5e84a13412dbad744485cbfb70e753a3a38dbe87
runette <runette@gmail.com>
2019-02-08 09:03:56 +0000

    Fixed cookie problem

--- static/functions.js
+++ static/functions.js
@@ -29,10 +29,15 @@
 		  signInSuccessWithAuthResult: function(authResult, redirectUrl){
 			user=authResult.user;
 			user.getIdToken().then(function (token) {
-			    document.cookie = "token=" + token
+			    setCookie("token", token)
+			    if (window.location.pathname.includes('/database/entry')) {location.reload(true)}
 			    })
 			return false
-		    }},
+		    },
+		    signInFailure(error){
+			return alert(stringify(error))
+		    }
+		    },
 		    
 		  signInOptions: [
 		    // Comment out any lines corresponding to providers you did not check in
@@ -51,14 +56,15 @@
 		};
 	if (ui.isPendingRedirect()){
 	    ui.start('#firebaseui-auth-container', uiConfig);
-	    $('#login').text("Logout")
+	    $('#login').text("Logout");
+	    
 	    }
 	$('#login').click(function(){
 	    if ($('#login').text().includes("Logout")){
 		firebase.auth().signOut()
-		document.cookie = "token=";
+		clearCookie('token');
 		$('#firebaseui-auth-container').text("You are now logged out")
-		$('[data-dismiss="modal"]').click(function(){location.reload(true)})
+		$('[data-dismiss="modal"]').click(function(){window.location.href ="/" })
 		}
 	    else {
 		firebase.auth().onAuthStateChanged(function (user) {
@@ -67,7 +73,7 @@
 		      $('#login').text("Logout")
 		      console.log(`Signed in as ${user.displayName} (${user.email})`);
 		    } else {
-		      document.cookie = "token=";
+		      //clearCookie('token');
 		      ;
 		    }
 		  }, function (error) {
@@ -303,6 +309,37 @@ function select_button ( cl) {
                 break;
         }
     }
+    
+    function setCookie(name,value){
+	createCookie(name, value, 1)
+	}
+	
+    function createCookie(name,value,days) {
+	if (days) {
+	    var date = new Date();
+	    date.setTime(date.getTime()+(days*24*60*60*1000));
+	    var expires = "; expires="+date.toGMTString();
+	    }
+	else var expires = "";
+	document.cookie = name + "="+value + expires + "; path=/";
+	}
+	
+    function clearCookie(name){
+	createCookie(name,"",-1);
+    }
+    
+    function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+	    var c = ca[i];
+	    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+	    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	    }
+	return null;
+	}
+	
+    
 
 
 
