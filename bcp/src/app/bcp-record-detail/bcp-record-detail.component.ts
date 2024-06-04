2af7ce29eff54894784316d654c48faa29f16cd5
Paul <runette@gmail.com>
2024-06-04 07:53:16 +0100

    fix measurement regression

--- bcp/src/app/bcp-record-detail/bcp-record-detail.component.ts
+++ bcp/src/app/bcp-record-detail/bcp-record-detail.component.ts
@@ -65,6 +65,7 @@ export class BcpRecordDetailComponent implements OnInit, OnDestroy {
         country: null,
         length: null,
         base_ring: null,
+        muzzle: null,
         bore: null,
         trunnion_position: null,
         trunnion_width:  null,
